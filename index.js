7f76f824be40dbeea30a2d837b82f9b9ddffb3df
Kem2523 <krystalemorris@gmail.com>
2022-10-09 23:04:11 -0500

    part 3

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-// Require needed modules.
+require('dotenv').config()
 const express = require('express')
-
-// Initialize the app object.
 const app = express()
 
-// Create a homepage route.
-app.get('/', function (req, res) {
-    res.send('Hello world')
+app.use('/places', require('./controllers/places'))
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
-// Create a second route.
-app.get('/second', function (req, res) {
-    res.send('My Second Page!')
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
-// Listen for connections.
-app.listen(3005, function () {
-})
-
-
-
-
+app.listen(process.env.PORT)
